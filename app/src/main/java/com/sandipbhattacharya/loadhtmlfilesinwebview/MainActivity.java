5e3c7831d37e82c819bb8d7eb2fe430690279e54
Alexandre Hostand <42520319+hostand@users.noreply.github.com>
2024-02-03 02:49:24 +0100

    Update MainActivity.java
    Added support to local file storage access and cookies management.

--- app/src/main/java/com/sandipbhattacharya/loadhtmlfilesinwebview/MainActivity.java
+++ app/src/main/java/com/sandipbhattacharya/loadhtmlfilesinwebview/MainActivity.java
@@ -3,6 +3,8 @@ package com.sandipbhattacharya.loadhtmlfilesinwebview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.CookieManager;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -17,10 +19,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         webView = findViewById(R.id.myWebView);
         WebSettings webSettings = webView.getSettings();
-        webSettings.setBuiltInZoomControls(true);
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setAllowContentAccess(true);
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
+        webView.setWebChromeClient(new WebChromeClient());
         webView.setWebViewClient(new Callback());
         webView.loadUrl("file:///android_asset/index.html");
+
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptCookie(true);
+        cookieManager.setAcceptThirdPartyCookies(webView,true);
     }
 
     private class Callback extends WebViewClient {
